13a4c276a3a9cf5e35a0d6115901d0387e291b3c
Sygris <tiago.a.vista@gmail.com>
2025-07-17 18:07:50 +0100

    Remade everything to make sure I know it

---
+++ src/app/page.tsx
@@ -0,0 +1,9 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <main className="flex flex-col gap-y-5 items-center pt-4 text-center">
+      Homepage
+    </main>
+  );
+}
