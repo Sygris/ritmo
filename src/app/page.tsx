b0ede918703033566dc769a3562c4f18520681b2
Sygris <tiago.a.vista@gmail.com>
2025-07-17 18:47:59 +0100

    Connected to database

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,16 @@
-import Image from "next/image";
+import { prisma } from "@/lib/prisma";
 
-export default function Home() {
+export default async function Home() {
+
+  const users = await prisma.user.findMany();
+
   return (
     <main className="flex flex-col gap-y-5 items-center pt-4 text-center">
-      Homepage
+      <ul>
+        {users.map((user) =>
+          <li key={user.id}>{user.username}</li>
+        )}
+      </ul>
     </main>
   );
 }
