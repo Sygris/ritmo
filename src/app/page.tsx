3cf9b15e98459a52cb7f296a26c8672a3d9a5463
Sygris <tiago.a.vista@gmail.com>
2025-07-17 19:08:06 +0100

    Added schema.prisma output back

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,5 @@
 import { prisma } from "@/lib/prisma";
+import Link from "next/link";
 
 export default async function Home() {
 
@@ -6,11 +7,9 @@ export default async function Home() {
 
   return (
     <main className="flex flex-col gap-y-5 items-center pt-4 text-center">
-      <ul>
-        {users.map((user) =>
-          <li key={user.id}>{user.username}</li>
-        )}
-      </ul>
+      {users.map((user) =>
+        <Link href={"./page.tsx"} key={user.id}>{user.username}</Link>
+      )}
     </main>
   );
 }
