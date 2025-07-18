0da0923dca743e1057281060a82a3098d164c83d
Sygris <tiago.a.vista@gmail.com>
2025-07-18 15:54:27 +0100

    Basic SideBar made

--- src/components/SideBar.tsx
+++ src/components/SideBar.tsx
@@ -1,31 +1,42 @@
 import Link from "next/link";
-import { Children } from "react";
+import { IconType } from "react-icons";
+import { 
+  HiOutlineArrowTrendingUp,
+  HiOutlineClipboard,
+  HiOutlineHome,
+  HiOutlineWrenchScrewdriver
+} from "react-icons/hi2";
 
 const links = [
-  { text: "Ritmo", href: "/" },
-  { text: "My Habits", href: "/habits "},
-  { text: "Statistics", href: "/settings" },
-  { text: "Settings", href: "/settings" }
+  { icon: HiOutlineHome,text: "Ritmo", href: "/" },
+  { icon: HiOutlineClipboard,text: "My Habits", href: "/habits "},
+  { icon: HiOutlineArrowTrendingUp,text: "Statistics", href: "/settings" },
+  { icon: HiOutlineWrenchScrewdriver,text: "Settings", href: "/settings" }
 ];
 
 export default function SideBar() {
   return (
-    <aside className="flex flex-col items-center justify-center gap-y-6 fixed
-    top-0 left-0 p-6 bg-emerald-400 h-screen">
+    <aside className="flex flex-col justify-center gap-y-10 fixed
+    top-0 left-0 p-6 bg-black/80 h-screen">
       {links.map((link) => {
-        return <NavLink key={link.text} href={link.href} text={link.text} />
+        return <NavLink key={link.text} href={link.href} icon={link.icon} text={link.text} />
       })}
     </aside>
   )
 }
 
 type NavLinkProps = {
+  icon: IconType,
   href: string,
   text: string
 }
 
-function NavLink({href, text}: NavLinkProps) {
+function NavLink({icon: Icon, href, text}: NavLinkProps) {
   return (
-    <Link href={`${href}`}>{text}</Link> 
+    <Link href={`${href}`} 
+      className="flex gap-x-2 text-black dark:text-white">
+      <Icon size={24}/>
+      {text}
+    </Link> 
   );
 }
