6e0bc734a552c3f1bf2299b05d950a1183d0ae95
Sygris <tiago.a.vista@gmail.com>
2025-07-18 18:27:32 +0100

    Created NavLink component for NavLinks

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -16,7 +16,7 @@ export default function RootLayout({
     <html lang="en">
       <body>
         <div className="flex h-screen flex-col md:flex-row md:overflow-hidden">
-          <div className="w-full flex-none md:w-64">
+          <div className="w-1 flex-none md:w-64">
             <SideNav />
           </div>
           <main className="flex-grow p-6 md:overflow-y-auto md:p-12">{children}</main>
--- src/components/NavLinks.tsx
+++ src/components/NavLinks.tsx
@@ -2,47 +2,55 @@
 import { usePathname } from 'next/navigation';
 import Link from 'next/link';
 import clsx from 'clsx';
-import { 
+import {
   HiOutlineArrowTrendingUp,
   HiOutlineClipboard,
   HiOutlineHome,
   HiOutlineWrenchScrewdriver
 } from 'react-icons/hi2';
+import { IconTree, IconType } from 'react-icons';
 
 const links = [
-  { icon: HiOutlineHome,text: 'Ritmo', href: '/' },
-  { icon: HiOutlineClipboard,text: 'My Habits', href: '/habits'},
-  { icon: HiOutlineArrowTrendingUp,text: 'Statistics', href: '/settings' },
-  { icon: HiOutlineWrenchScrewdriver,text: 'Settings', href: '/settings' }
+  { icon: HiOutlineHome, text: 'Ritmo', href: '/' },
+  { icon: HiOutlineClipboard, text: 'My Habits', href: '/habits' },
+  { icon: HiOutlineArrowTrendingUp, text: 'Statistics', href: '/settings' },
+  { icon: HiOutlineWrenchScrewdriver, text: 'Settings', href: '/settings' }
 ];
 
 export default function NavLinks() {
-  const pathName = usePathname();
 
   return (
     <>
-      {links.map((link) => {
-        const LinkIcon = link.icon;
+      <NavLink href='/' icon={HiOutlineHome} text='Dashboard' />
+      <NavLink href='/habits' icon={HiOutlineClipboard} text='My Habits' />
+      <NavLink href='/stats' icon={HiOutlineArrowTrendingUp} text='Statistics' />
+      <NavLink href='/settings' icon={HiOutlineWrenchScrewdriver} text='Settings' />
+    </>
+  );
+}
 
-        return (
-        <Link 
-          key={link.text} 
-          href={link.href}
-          className={clsx(
-            `flex h-[48px] grow items-center justify-center gap-2 rounded-md
+function NavLink({ href, icon: Icon, text }: { href: string, icon: IconType, text: string }) {
+  const pathName = usePathname();
+  const isActive = pathName == href;
+
+  return (
+    <Link
+      href={href}
+      className={clsx(
+        `flex h-[48px] grow items-center justify-center gap-2 rounded-md
           bg-gray-50 p-3 text-sm font-medium hover:bg-sky-100 hover:text-blue-600 
             md:flex-none md:justify-start md:p-2 md:px-6`,
-            {
-              'bg-sky-100 text-blue-600': pathName === link.href,
-              'after:absolute after:right-0 after:content-[""] after:block after:bg-blue-600 after:w-2 after:h-8 after:rounded-l-sm': pathName === link.href,
-            }
-          )}
-        >
-          <LinkIcon size={24} className='w-6'/>
-          <p>{link.text}</p>
-        </Link>
-        )
-      })}
-    </>
+        {
+          'bg-sky-100 text-blue-600': isActive,
+        }
+      )}
+    >
+      <Icon size={24} className='w-6' />
+      <p className='hidden md:block'>{text}</p>
+
+      {isActive && (
+        <span className='absolute right-0 w-1.5 h-8 bg-blue-500 rounded-l-lg' />
+      )}
+    </Link>
   );
 }
