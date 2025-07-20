3141e6b1328b6d1053a6189292a899b8d5c48e75
Sygris <tiago.a.vista@gmail.com>
2025-07-20 21:14:41 +0100

    Installed Zod

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,7 +1,33 @@
-import { GalleryVerticalEnd } from "lucide-react"
+import { 
+  Card,
+  CardContent,
+  CardHeader,
+  CardTitle
+} from "../ui/Card"
 
 export default function LoginPage() {
   return (
-    <h1>Login</h1>
+    <div className="flex justify-center items-center h-screen">
+      <Card>
+        <CardHeader>
+          <CardTitle className="text-center">
+            Welcome
+          </CardTitle>
+        </CardHeader>
+        <CardContent>
+          <form action="" method="post">
+            <div className="flex flex-col items-start">
+              <label htmlFor="email">Email</label>
+              <input type="email" name="email" id="email" required placeholder="your@email.com" />
+            </div>
+            <div className="flex flex-col items-start">
+              <label htmlFor="password">Password</label>
+              <input type="password" name="password" id="password" required placeholder="Your password" />
+            </div>
+            <button type="submit">Login</button>
+          </form>
+        </CardContent>
+      </Card>
+    </div>
   )
 }
