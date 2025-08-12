4b6c76aa2c42ca7acf07369e6e3f88cd43e3326a
Sygris <tiago.a.vista@gmail.com>
2025-08-12 16:30:10 +0100

    Im so confused rn

--- src/app/ui/Components/AddNewHabitModal.tsx
+++ src/app/ui/Components/AddNewHabitModal.tsx
@@ -1,12 +1,20 @@
+import { createUser } from "@/lib/data";
 import Modal from "./Modal";
+import React from "react";
 
 export default function AddNewHabitModal() {
+
+  const handleSubmit = (e: React.SyntheticEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    createUser();
+  }
+
   return (
     <Modal title="Add New Habit">
-      <form action="" method="post" className="space-y-4 mt-4 p-2">
+      <form onSubmit={handleSubmit} className="space-y-4 mt-4 p-2">
         <div className="flex flex-col items-start justify-center gap-2">
           <label htmlFor="habitName">Habit name</label>
-          <input type="text" name="habitName" id="habitName" placeholder="e.g., Morning Meditation" className="self-stretch rounded-md shadow-xs"/>
+          <input type="text" name="habitName" id="habitName" placeholder="e.g., Morning Meditation" className="self-stretch rounded-md shadow-xs" />
         </div>
         <div className="flex flex-col items-start justify-center gap-2">
           <label htmlFor="habitDescription">Description</label>
--- src/lib/data.ts
+++ src/lib/data.ts
@@ -1,4 +1,17 @@
+'use server'
 import { prisma } from "./prisma";
 
-function fetchTodaysHabits() {
+export async function createUser() {
+  const newUser = await prisma.user.create({
+    data: {
+      email: 'tiago.a.vista@gmail.com',
+      name: 'Tiago Vista'
+    },
+    select: {
+      id: true,
+      email: true
+    }
+  });
+
+  console.log(newUser);
 }
